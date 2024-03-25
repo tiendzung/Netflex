c0695e51c8268c89255c7d66594947c60797e5f9
tiendzung <tiendung050803@gmail.com>
2024-03-25 12:35:43 +0700

    feat: Detail Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:mobile/models/profile.dart';
-import 'package:mobile/screens/profiles_more.dart';
+import 'package:mobile/details.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Netflix',
+      title: 'Flutter Netflix UI',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        fontFamily: 'Netflix Sans',
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: Colors.black,
       ),
-      home: ProfileScreen(
-        profiles: demoProfile[0],
-      ),
-      debugShowCheckedModeBanner: false,
+      home: Detail(),
     );
   }
 }
