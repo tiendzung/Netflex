189408850f2cf17b441bfe4fc84eb913daaea057
tiendzung <tiendung050803@gmail.com>
2024-04-03 15:35:13 +0700

    update: screen

--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -50,7 +50,7 @@ class CustomAppBar extends StatelessWidget {
 
 class _AppBarButton extends StatelessWidget {
   final String title;
-  final Function onTap;
+  final VoidCallback onTap;
 
   const _AppBarButton({
     Key? key,
@@ -61,7 +61,7 @@ class _AppBarButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: onTap(),
+      onTap: onTap,
       child: Text(
         title,
         style: TextStyle(
